548a82bf250ee27ae1455def75019073c4183003
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-03-09 20:53:58 +0300

    [ADD} footer

--- my-portfolio/src/app/layout/main-layout/main-layout.ts
+++ my-portfolio/src/app/layout/main-layout/main-layout.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Header } from '../header/header';
 import { RouterModule } from '@angular/router';
+import { Footer } from '../footer/footer';
 
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [Header, RouterModule],
+  imports: [Header, RouterModule, Footer],
   templateUrl: './main-layout.html',
  
 })
