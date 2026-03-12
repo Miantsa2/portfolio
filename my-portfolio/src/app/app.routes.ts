6a4a49954624cf77250a036bcee72a61f3b9bd1a
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-03-12 18:57:27 +0300

    [ADD] home page with download CV

--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Header } from './layout/header/header';
 import { MainLayout } from './layout/main-layout/main-layout';
+import { Profile } from './pages/profile/profile';
 export const routes: Routes = [
- { path: '', redirectTo: 'home', pathMatch: 'full' },
+ { path: '', redirectTo: 'main-layout', pathMatch: 'full' },
  { path: 'header', component: Header }, 
  { path: 'main-layout', component: MainLayout ,
      children: [
@@ -15,6 +16,11 @@ export const routes: Routes = [
       {
         path: 'home',
         component: Home
+      },
+
+      {
+        path: 'profile',
+        component: Profile
       },
      ]
  }, 
