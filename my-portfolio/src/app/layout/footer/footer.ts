8cff5b0e84a8241cf186681dc7d77f83eda7b593
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-07-11 19:15:49 +0300

    [FIX] translation in footer

--- my-portfolio/src/app/layout/footer/footer.ts
+++ my-portfolio/src/app/layout/footer/footer.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+
+}
